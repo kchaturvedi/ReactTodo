dcf0dce47fd4e48c4450f1a1babb129c68f2d858
Kartik Chaturvedi <kartikchaturvedi1@gmail.com>
2016-05-11 22:38:19 -0400

    Test search component

---
+++ app/tests/components/TodoSearch.test.jsx
@@ -0,0 +1,34 @@
+let React = require('react')
+let ReactDOM = require('react-dom')
+let TestUtils = require('react-addons-test-utils')
+let expect = require('expect')
+let $ = require('jQuery')
+
+let TodoSearch = require('TodoSearch')
+
+describe('TodoSearch', () => {
+  it('should exist', () => {
+    expect(TodoSearch).toExist()
+  })
+  
+  it('should call onSearch with entered input text', () => {
+    let searchText = 'Dog'
+    let spy = expect.createSpy()
+    let todoSearch = TestUtils.renderIntoDocument(<TodoSearch onSearch={spy}/>)
+    
+    todoSearch.refs.searchText.value = searchText
+    TestUtils.Simulate.change(todoSearch.refs.searchText)
+    
+    expect(spy).toHaveBeenCalledWith(false, 'Dog')
+  })
+  
+  it('should call onSearch with proper checked value', () => {
+    let spy = expect.createSpy()
+    let todoSearch = TestUtils.renderIntoDocument(<TodoSearch onSearch={spy}/>)
+    
+    todoSearch.refs.showCompleted.checked = true
+    TestUtils.Simulate.change(todoSearch.refs.showCompleted)
+    
+    expect(spy).toHaveBeenCalledWith(true, '')
+  })
+})
