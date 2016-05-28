d2ba616b7332f13db53618ac52202c603357b61a
Kartik Chaturvedi <kartikchaturvedi1@gmail.com>
2016-05-28 18:37:00 -0400

    Fetch todos from firebase on app start

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -11,49 +11,6 @@ describe('TodoAPI', () => {
     expect(TodoAPI).toExist()
   })
   
-  describe('setTodos', () => {
-    it('should set valid todos array', () => {
-      let todos = [{
-        id: 23,
-        text: 'test all files',
-        completed: false
-      }]
-      TodoAPI.setTodos(todos)
-      
-      let actualTodos = JSON.parse(localStorage.getItem('todos'))
-      
-      expect(actualTodos).toEqual(todos)
-    })
-    
-    it('should not set invalid todos array', () => {
-      let badTodos = {a: 'b'}
-      TodoAPI.setTodos(badTodos)
-      
-      expect(localStorage.getItem('todos')).toBe(null)
-    })
-  })
-  
-  describe('getTodos', () => {
-    it('should return empty array for bad localStorage data', () => {
-      let actualTodos = TodoAPI.getTodos()
-      
-      expect(actualTodos).toEqual([])
-    })
-    
-    it('should return todos if valid array in localStorage', () => {
-      let todos = [{
-        id: 23,
-        text: 'test all files',
-        completed: false
-      }]
-      localStorage.setItem('todos', JSON.stringify(todos))
-      
-      let actualTodos = TodoAPI.getTodos()
-      
-      expect(actualTodos).toEqual(todos)
-    })
-  })
-  
   describe('filterTodos', () => {
     let todos = [{
       id: 1,
