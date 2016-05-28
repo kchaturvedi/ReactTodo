270d0082bdd023d18a972c5b43e8f08b3312caba
Kartik Chaturvedi <kartikchaturvedi1@gmail.com>
2016-05-28 18:13:11 -0400

    Update todos on firebase

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -62,7 +62,7 @@ describe('Reducers', () => {
       expect(res[0]).toEqual(todos[0])
     })
     
-    it('should toggle todo', () => {
+    it('should update todo', () => {
       let todos = [{
         id: '123',
         text: 'Something',
@@ -70,14 +70,20 @@ describe('Reducers', () => {
         createdAt: 123,
         completedAt: 125
       }]
+      let updates = {
+        completed: false,
+        completedAt: null
+      }
       let action = {
-        type: 'TOGGLE_TODO',
-        id: '123'
+        type: 'UPDATE_TODO',
+        id: todos[0].id,
+        updates
       }
       let res = reducers.todosReducer(df(todos), df(action))
       
-      expect(res[0].completed).toEqual(false)
-      expect(res[0].completedAt).toEqual(undefined)
+      expect(res[0].completed).toEqual(updates.completed)
+      expect(res[0].completedAt).toEqual(updates.completedAt)
+      expect(res[0].text).toEqual(todos[0].text)
     })
   })
 })
